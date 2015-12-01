ba87c70a85501f7ab0baf0bacf850facc238b50f
Nickduino <Nickduino@users.noreply.github.com>
2015-12-01 18:19:11 +0100

    Added the EEPROM stuff

--- Somfy_Remote.ino
+++ Somfy_Remote.ino
@@ -1,19 +1,33 @@
-#define PORT_TX 6 //5 of PORTD = DigitalPin 5
+/*  This sketch allows you to simulate a Somfy RTS or Simu HZ remote.
+   The rolling code will be stored in EEPROM, so that you can power the Arduino off.
+   
+   Easiest way to make it work for you:
+    - Choose a remote number
+    - Choose a starting point for the rolling code. Any unsigned int works, 1 is a good start
+    - Upload the sketch
+    - Long-press the program button of YOUR ACTUAL REMOTE until your blind goes up and down slightly
+    - send 'p' to the serial terminal
+  To make a group command, just repeat the last two steps with another blind (one by one)
+*/
+
+#include <EEPROM.h>
+#define PORT_TX 5 //5 of PORTD = DigitalPin 5
 
 #define SYMBOL 640
 #define HAUT 0x2
 #define STOP 0x1
 #define BAS 0x4
 #define PROG 0x8
+#define EEPROM_ADDRESS 0
 
-#define REMOTE 0x696120    //<-- Change it!
+#define REMOTE 0x698140    //<-- Change it!
 
-int rollingCode = 1;       //<-- Change it!
-
+unsigned int newRollingCode = 101;       //<-- Change it!
+unsigned int rollingCode = 0;
 byte frame[7];
 byte checksum;
 
-void BuildFrame(byte *frame, int* code, byte button);
+void BuildFrame(byte *frame, byte adresse, byte button);
 void SendCommand(byte *frame, byte sync);
 
 
@@ -22,32 +36,40 @@ void setup() {
   DDRD |= 1<<PORT_TX; // Pin 5 an output
   PORTD &= !(1<<PORT_TX); // Pin 5 LOW
 
-/*  BuildFrame(frame, HAUT);
-  SendCommand(frame, 2);
-*/   
+  if (EEPROM.get(EEPROM_ADDRESS, rollingCode) < newRollingCode) {
+    EEPROM.put(EEPROM_ADDRESS, newRollingCode);
+  }
+  Serial.print("Simulated remote number : "); Serial.println(REMOTE, HEX);
+  Serial.print("Current rolling code    : "); Serial.println(rollingCode);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     char serie = (char)Serial.read();
     Serial.println("");
+//    Serial.print("Remote : "); Serial.println(REMOTE, HEX);
     if(serie == 'm'||serie == 'u'||serie == 'h') {
       Serial.println("Monte");
-      BuildFrame(frame, &rollingCode, HAUT);
+      BuildFrame(frame, HAUT);
     }
     else if(serie == 's') {
       Serial.println("Stop");
-      BuildFrame(frame, &rollingCode, STOP);
+      BuildFrame(frame, STOP);
     }
     else if(serie == 'b'||serie == 'd') {
       Serial.println("Descend");
-      BuildFrame(frame, &rollingCode, BAS);
+      BuildFrame(frame, BAS);
     }
     else if(serie == 'p') {
       Serial.println("Prog");
-      BuildFrame(frame, &rollingCode, PROG);
+      BuildFrame(frame, PROG);
     }
-    delay(500); Serial.println("");
+    else {
+      Serial.println("Custom");
+      BuildFrame(frame, serie);
+    }
+    delay(500); // Is it useful?
+    Serial.println("");
     SendCommand(frame, 2);
     for(int i = 0; i<4; i++) {
       SendCommand(frame, 7);
@@ -56,22 +78,24 @@ void loop() {
 }
 
 
-void BuildFrame(byte *frame, int* code, byte button) {
-//  Serial.print("Pointeur Code : "); Serial.println(*code);
+void BuildFrame(byte *frame, byte button) {
+  unsigned int code;
+  EEPROM.get(EEPROM_ADDRESS, code);
   frame[0] = 0xA7;
   frame[1] = button << 4;
-  frame[2] = ((*code) &0xFF00) >> 8; // Rolling code
-  frame[3] = (*code) & 0xFF;      // Rolling code
+  frame[2] = code >> 8; // Rolling code
+  frame[3] = code;      // Rolling code
   frame[4] = REMOTE >> 16;
   frame[5] = REMOTE >> 8;
   frame[6] = REMOTE;
 
-
-  Serial.println(""); Serial.print("Original ");
+  Serial.print("Frame      : ");
   for(byte i = 0; i < 7; i++) {
-    Serial.print(frame[i],HEX);
+    if(frame[i] >> 4 == 0) {
+      Serial.print("0");
+    }
+    Serial.print(frame[i],HEX); Serial.print(" ");
   }
-
   
 // Checksum calculation
   checksum = 0;
@@ -81,22 +105,31 @@ void BuildFrame(byte *frame, int* code, byte button) {
   }
   checksum = checksum & 0b1111; // We keep the last 4 bits only
 
-  Serial.println("");
-  Serial.print("Checksum RAW : "); Serial.println(checksum);
+
 //Checksum integration
   frame[1] |= checksum;
-  Serial.print("Checksum après : ");Serial.println(frame[1] & 0xF);
 
+  Serial.println(""); Serial.print("Frame+Chk  : ");
+  for(byte i = 0; i < 7; i++) {
+    if(frame[i] >> 4 == 0) {
+      Serial.print("0");
+    }
+    Serial.print(frame[i],HEX); Serial.print(" ");
+  }
+
+  
 // Obfuscation
   for(byte i = 1; i < 7; i++) {
     frame[i] = frame[i] ^ frame[i-1];
   }
 
-  Serial.println(""); Serial.print("Obfuscated ");
+  Serial.println(""); Serial.print("Obfuscated : ");
   for(byte i = 0; i < 7; i++) {
-    Serial.print(frame[i],HEX);
+    Serial.print(frame[i],HEX); Serial.print(" ");
   }
-  (*code)++; // The rolling code is incremented
+  Serial.println("");
+  Serial.print("Rolling Code : "); Serial.println(code);
+  EEPROM.put(EEPROM_ADDRESS, code + 1);
 }
 
 void SendCommand(byte *frame, byte sync) {
@@ -128,7 +161,7 @@ void SendCommand(byte *frame, byte sync) {
   
 //Data
 for(byte i = 0; i < 57; i++) {
-    if((frame[i/8] >> (7 - (i%8))) & 1 == 1) {
+    if(((frame[i/8] >> (7 - (i%8))) & 1) == 1) {
       PORTD &= !(1<<PORT_TX);
       delayMicroseconds(SYMBOL);
       PORTD ^= 1<<5;
